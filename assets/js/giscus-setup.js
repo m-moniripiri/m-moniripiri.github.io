bf228af113799914a1f0a040df9d6266e0337476
m-moniripiri <m-moniripiri@users.noreply.github.com>
2026-04-18 07:51:32 +0000

    Deploying to gh-pages from @ m-moniripiri/m-moniripiri.github.io@af82cce72a2b6a46ba80ed89e8f13bf82b60890d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "m-moniripiri/m-moniripiri",
+    "data-repo": "m-moniripiri/m-moniripiri.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
